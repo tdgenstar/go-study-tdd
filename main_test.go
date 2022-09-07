77318b611ebe124b38c0493fc4f0c2402f637c9e
tdgenstar <tdgenstar@gmail.com>
2022-09-07 20:39:37 +0900

    test(hello world): 스페인어를 쓰고 이름이 비어있다면 Hola, world

--- main_test.go
+++ main_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	type args struct {
-		name string
+		name     string
+		language string
 	}
 	tests := []struct {
 		name string
@@ -14,21 +15,31 @@ func TestHello(t *testing.T) {
 		{
 			name: "it should print hello, world",
 			args: args{
-				name: "Teddy",
+				name:     "Teddy",
+				language: "",
 			},
 			want: "Hello, Teddy",
 		},
 		{
 			name: "it should empty name, then print 'World'",
 			args: args{
-				name: "",
+				name:     "",
+				language: "",
 			},
 			want: "Hello, World",
+		},
+		{
+			name: "it should language == spanish and empty name, then Hola, world",
+			args: args{
+				name:     "",
+				language: "",
+			},
+			want: "Hola, World",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Hello(tt.args.name); got != tt.want {
+			if got := Hello(tt.args.name, tt.args.language); got != tt.want {
 				t.Errorf("Hello() = %v, want %v", got, tt.want)
 			}
 		})
