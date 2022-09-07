b42108c3dc58de223cbf7a63f063ed953c735bfe
TedyChang <teddychangstar@gmail.com>
2022-09-07 20:14:09 +0900

    test: add parameter name, hello world

--- main_test.go
+++ main_test.go
@@ -3,18 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+	type args struct {
+		name string
+	}
 	tests := []struct {
 		name string
+		args args
 		want string
 	}{
 		{
 			name: "it should print hello, world",
-			want: "Hello, world",
+			args: args{
+				name: "Teddy",
+			},
+			want: "Hello, Teddy",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Hello(); got != tt.want {
+			if got := Hello(tt.args.name); got != tt.want {
 				t.Errorf("Hello() = %v, want %v", got, tt.want)
 			}
 		})
